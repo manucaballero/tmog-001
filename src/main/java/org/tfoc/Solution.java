461da955bb8638fb90b9497f94b4077e88aa6ba4
Manuel Caballero Rabago <mcabalra@mercadona.es>
2024-02-09 16:03:53 +0100

    feat: Implemented O(n^2) solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -5,11 +5,29 @@ package org.tfoc;
  */
 public class Solution {
 
-    public int[] twoSum(
+    public static int[] twoSumV1(
             int[] nums,
             int target) {
 
-        return null;
+        boolean isFound = false;
+        int[] response = new int[2];
+        int counter = 0;
+        while (!isFound && counter < nums.length - 1){
+            int currentNumber = nums[counter];
+            int secondPointer = counter + 1;
+            while (!isFound && secondPointer < nums.length){
+                int secondNumber = nums[secondPointer];
+                if(currentNumber + secondNumber == target){
+                    isFound = true;
+                    response[0] = counter;
+                    response[1] = secondPointer;
+                }
+                secondPointer++;
+            }
+            counter++;
+        }
+
+        return response;
     }
 
 }
